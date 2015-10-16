27adc7ac3db21a2c74343ee99bb5eac8fe3aad16
Vincent Wu <wuv21@uw.edu>
2015-10-15 17:07:26 -0700

    10.15.15 class

--- jquery/js/app.js
+++ jquery/js/app.js
@@ -6,3 +6,19 @@
     http://api.openweathermap.org/data/2.5/weather?zip=98195,us&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f
 */
 
+$(function() {
+    'use strict';
+
+    $('a').attr('target', '_blank');
+    $('article').hide().fadeIn(500);
+
+    $('#toggle-article').click(function() {
+       $('article').fadeToggle();
+    });
+
+    var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?zip=98195,us&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f";
+    $.getJSON(weatherUrl).then(function(data) {
+        $('#temp').text(Math.round(data['main']['temp']));
+    });
+});
+
