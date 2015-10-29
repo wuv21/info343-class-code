704fcdf384a42def28e1e745a206e2fdaf0981d4
Vincent Wu <wuv21@uw.edu>
2015-10-29 16:57:34 -0700

    10/29/15

--- angular/js/movies.js
+++ angular/js/movies.js
@@ -1,8 +1,32 @@
 
 angular.module('Movies', [])
     .controller('MoviesController', function($scope, $http) {
+        var ratingsMap = {
+            'Not Rated': 0,
+            'G': 1,
+            'PG': 2,
+            'PG-13': 3,
+            'R': 4,
+            'NC-17': 5,
+            'X': 6
+        };
+
         $http.get('../angular/data/movies-2014.json')
             .then(function(result) {
-                $scope.movies = result.data;
+                $scope.movies = result.data.map(function(movie) {
+                    movie.ratingOrdinal = ratingsMap[movie.rating];
+                    return movie;
+                });
+
+                $scope.distributors = _.uniq(_.pluck($scope.movies, 'distributor'));
             });
+
+        $scope.setSort = function(propertyName) {
+            if ($scope.sortCol === propertyName) {
+                $scope.sortReverse = !$scope.sortReverse;
+            } else {
+                $scope.sortCol = propertyName;
+                sortReverse = false;
+            }
+        }
     });
